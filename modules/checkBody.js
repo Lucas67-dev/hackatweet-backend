3f3df7ff4a1c13aca500b4e4109d1db9a73dd84b
LucasM <martzlucas67210@gmail.com>
2025-02-13 15:15:12 +0100

    get

--- modules/checkBody.js
+++ modules/checkBody.js
@@ -11,5 +11,3 @@ function checkBody(body, keys) {
   }
   
   module.exports = { checkBody };
-  
-  
--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,17 @@ const { checkBody } = require('../modules/checkBody');
 const bcrypt = require ('bcrypt');
 const uid2 = require ('uid2');
 
-// Route pour l'inscription
+/* Route pour l'inscription */
 router.post("/signup", (req, res) => {
   if (!checkBody(req.body, ['username', 'firstname', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' }); // Erreur dans les champs 
+    res.json({ result: false, error: 'Missing or empty fields' }); // Erreur dans les champs donc c'est false
     return;
   }
 
 
   User.findOne({ username: req.body.username }) // Cherche dans la BDD un meme nom
   .then(data => {
-    if (data === null) { // Pas de meme nom alors on crée
+    if (data === null) { // Pas de meme nom alors on crée un user
       const hash = bcrypt.hashSync(req.body.password, 10);
 
       const newUser = new User({ // création d'un nouvel User
@@ -29,32 +29,42 @@ router.post("/signup", (req, res) => {
         canBookmark: true,
       });
 
-      newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc });
+      newUser.save().then(newDoc => { // Sauvegarde le User dans la BDD
+        res.json({ result: true, token: newDoc.token }); // Si c'est true alors c'est sauvegarde
       });
     } else {
-      // L'utilisateur existe déjà dans la base de données
-      res.json({ result: false, error: '❌User already exists' });
+      res.json({ result: false, error: '❌User already exists' }); // L'utilisateur existe déjà dans la base de données donc cest false
     }
   });
 });
 
 
-// Route pour la connexion
+/* Route pour la connexion */
 router.post("/signin", (req, res) => {
   if (!checkBody(req.body, ['username', 'firstname', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
 
-  User.findOne({ username: req.body.username, firstname: req.body.firstname, password: req.body.password })
+  User.findOne({ username: req.body.username, firstname: req.body.firstname })
   .then(data => {
-    if (data) {
-      res.json({ result: true }); // User est enregistré 
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token }); // User est enregistré 
     } else {
       res.json ({ result: false, error: "❌User not found" }) // User pas trouvé
     }
   })
+});
+
+/* Route Get pour trouver le token d'un user */
+router.get('/canBookmark/:token', (req, res) => {
+  User.findOne({ token: req.params.token }).then(data => { // Recherche dans la BDD avec le token en parametre 
+    if (data) {
+      res.json({ result: true, canBookmark: data.canBookmark });
+    } else {
+      res.json({ result: false, error: 'User not found' });
+    }
+  });
 });
 
 module.exports = router;
